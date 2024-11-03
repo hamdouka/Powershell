7eff9ca81e6094a73dfdf192f3feb1c028906c65
hamdouka <47436664+hamdouka@users.noreply.github.com>
2024-11-03 18:47:41 -0400

    Add files via upload

---
+++ Cred_Management.ps1
@@ -0,0 +1,10 @@
+﻿$key=New-Object Byte[] 32
+[security.cryptography.RNGCryptoserviceprovider]::Create().Getbytes($key)
+
+$key  | Out-File C:\ps\aes.key
+
+(Get-Credential).Password | ConvertFrom-SecureString -Key (Get-Content C:\ps\aes.key) | Set-Content 'C:\ps\password.txt'
+$password=Get-Content C:\ps\password.txt |ConvertTo-SecureString -key (Get-Content C:\ps\aes.key)
+$credentials=New-Object System.Management.Automation.PSCredential("appuser",$password)
+
+Get-Service -ComputerName vm1 -credential $credentials
