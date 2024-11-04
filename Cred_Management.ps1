0074d986a5a2549cad970a1d5381f8a56087b5b6
hamdouka <47436664+hamdouka@users.noreply.github.com>
2024-11-03 20:39:51 -0400

    Update Cred_Management.ps1

--- Cred_Management.ps1
+++ Cred_Management.ps1
@@ -1,10 +1,13 @@
-﻿$key=New-Object Byte[] 32
+#This PowerShell script demonstrates secure credential management for remote system administration. 
+# 1. Generate AES Key
+$key = New-Object Byte[] 32
 [security.cryptography.RNGCryptoserviceprovider]::Create().Getbytes($key)
-
-$key  | Out-File C:\ps\aes.key
-
+$key | Out-File C:\PS\aes.key
+# 2. Store Encrypted Credentials
 (Get-Credential).Password | ConvertFrom-SecureString -Key (Get-Content C:\ps\aes.key) | Set-Content 'C:\ps\password.txt'
-$password=Get-Content C:\ps\password.txt |ConvertTo-SecureString -key (Get-Content C:\ps\aes.key)
-$credentials=New-Object System.Management.Automation.PSCredential("appuser",$password)
+# 3. Load and Use Credentials
+$password = Get-Content C:\ps\password.txt | ConvertTo-SecureString -key (Get-Content C:\ps\aes.key)
+$credentials = New-Object System.Management.Automation.PSCredential("Labadmin",$password)
 
-Get-Service -ComputerName vm1 -credential $credentials
+# 4. Query Remote System
+Get-WmiObject -Class win32_operatingsystem -ComputerName CM1 -Credential $credential
